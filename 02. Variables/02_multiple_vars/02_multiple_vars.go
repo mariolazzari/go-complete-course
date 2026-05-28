eefb656856220d0c1ec24dc73046f3595451c269
Mario Lazzari <mario.lazzari@gmail.com>
2026-05-28 09:50:32 +0200

    Done lesson 23

---
+++ 02. Variables/02_multiple_vars/02_multiple_vars.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare and initialize with var with explicit type
+	var coffeeName string = "Espresso"
+
+	// Type inferred
+	var size = "Small"
+
+	// Short declaration and initialization. Possible only inside functions
+	price := 2.50
+
+	fmt.Println("Small Espresso price is $2.50")
+	fmt.Println(size, coffeeName, "price is $", price)
+	fmt.Printf("%s %s price is $%.2f\n", size, coffeeName, price)
+}
--- README.md
+++ README.md
@@ -32,3 +32,7 @@ func main() {
 	color.Yellow("I want a cup of coffee!")
 }
 ```
+
+## Variables
+
+##
